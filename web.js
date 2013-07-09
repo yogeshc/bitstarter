e286581f89abbc1ac7c66e18503a1d7420d2653e
root <root@domU-12-31-39-09-AA-2B.compute-1.internal>
2013-07-09 15:17:35 +0000

    Changed to display contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('buffer.toString()');
 });
 port = process.env.PORT || 5000;
 app.listen(port, function() {
