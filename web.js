8f65e582debb9f0654922022448a5ecd1473f113
root <root@domU-12-31-39-09-AA-2B.compute-1.internal>
2013-07-09 15:28:53 +0000

    Corrected error in web.js to return index.html

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-
-var buffer = new Buffer(fs.readFileSync('index.html');
+var data = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('buffer.toString()');
+  response.send(data.toString());
 });
 port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
